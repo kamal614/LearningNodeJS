d3b6b1769bf1ca5b6a1d123437088f2be55549d7
MrKamal614 <kamal.k@softserv.in>
2022-09-07 00:34:14 +0530

    working on vid11

--- CRUD/myModules/index.js
+++ CRUD/myModules/index.js
@@ -1,3 +1,4 @@
-const oper = require('./oper');
-console.log(oper.add(5,5));
-console.log(oper.sub(5,5));
+const {add, sub, multi} = require('./oper');
+console.log(add(5,5));
+console.log(sub(5,5));
+console.log(multi(5,5));
--- CRUD/myModules/oper.js
+++ CRUD/myModules/oper.js
@@ -6,5 +6,9 @@ const sub = (a,b) => {
     return a-b;
 }
 
+const multi = (a,b) => {
+    return a*b;
+}
 module.exports.add = add;
 module.exports.sub = sub;
+module.exports.multi = multi;
